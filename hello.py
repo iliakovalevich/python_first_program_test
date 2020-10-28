415324ba5974b1f2e27c858c527a78be60d6eb6e
iliakovalevich <kava2000.ilia@gmail.com>
2020-10-28 21:28:12 +0300

    add first program

---
+++ hello.py
@@ -0,0 +1,55 @@
+# number = int(input("Input number:"))
+# i = 1
+# factorial = 1
+# while i <= number:
+#     factorial *= i
+#     i += 1
+# print("Factorial=", factorial)
+
+
+# --------------------------------------
+
+
+# number = int(input("Input number:"))
+# factorial=1
+# for i in range(1, number+1):
+#     factorial*=i
+# print(factorial)
+
+
+# --------------------------------------
+
+
+# for i in range(1,6):
+#     print()
+#     for j in range(i,x):
+#         print(i*j, end="\t")
+
+# ---------------------------------------
+
+
+# # Обменный пункт
+# print("Для выхода нажмите Y")
+# # Курсы
+# usd = 2.62
+# eur = 3.05
+# while True:
+#     data = input("Input sum for currency:")
+#     if data.lower() == "y":
+#         break  # выход из цикла
+#     money = int(data)
+#     valute = int(input("Input type of valute (usd=400/eur=500):"))
+#     if valute == 400:
+#         course = usd
+#     elif valute == 500:
+#         course = eur
+#     else:
+#         print("valute is not found!")
+#         continue
+#     cache = money / course
+#     print(cache)
+# print("finish")
+
+# ---------------------------------------
+
+
